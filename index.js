9e3622dd3e4897e20cca0130aa19ff297ce608ea
SyedEbad902 <ebad3088@gmail.com>
2024-10-15 03:31:42 +0500

    create a new task

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Get the current file's path
+const __filename = fileURLToPath(import.meta.url);
+// Get the directory name of the current file
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", function (req, res) {
+  fs.readdir("./files", function (err, files) {
+    console.log(files);
+    res.render("index", { files: files });
+  });
+});
+
+app.get('/files/:filename', function (req, res) {
+  fs.readFile(`./files/${req.params.filename}`, "utf-8", function (err, filedata) {
+    console.log(filedata);
+      res.render("showFile", { fileName: req.params.filename , fileData : filedata });
+  });
+});
+
+app.post("/create", function (req, res) {
+  fs.writeFile(
+    `./files/${req.body.title.split(" ").join("")}.txt`,
+    req.body.taskDetails,
+    function (err) {
+      console.log(err);
+      res.redirect("/");
+    }
+  );
+});
+app.listen(3000);
